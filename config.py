d44577182bcb65dfbff756e11aa8f02eb4f6a3c4
GOATGOD8 <nathansierra48@gmail.com>
2026-04-23 20:36:39 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+# config.py
 
+# Each parking spot is (x, y, width, height)
+PARKING_SPOTS = [
+    (100, 200, 80, 160),
+    (200, 200, 80, 160),
+    (300, 200, 80, 160),
+    (400, 200, 80, 160)
+]
